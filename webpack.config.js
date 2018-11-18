ec40c3fa0cfce8cb62df76278c897ef8f877ebc3
andy <thamandyy@gmail.com>
2018-11-18 13:20:39 -0500

    Webpack

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+
+      {
+        test: /\.js[x]?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+        
+      },
+      {
+        test:/\.(s*)css$/,
+        use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../'
+            }
+          },
+          "css-loader"
+				]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif|ico)$/,
+        use: [{
+            loader: 'url-loader',
+            options: {
+                limit: 8000, // Convert images < 8kb to base64 strings
+                name: 'images/[hash]-[name].[ext]'
+            }
+        }]
+      }
+    ]
+  },
+  plugins: [
+		new MiniCssExtractPlugin('style.css'),
+		new HtmlWebpackPlugin(
+			{
+				hash: false,
+				template: "./index.html"
+			}
+		)
+  ]
+};
+
+module.exports = config
